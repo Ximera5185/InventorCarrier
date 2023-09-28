452821254a43fd629b959be0090203c42e39975c
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2023-09-28 03:55:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorCarrier
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+        }
+    }
+}
